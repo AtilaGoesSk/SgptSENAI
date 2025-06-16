c9154e7b3ecfea3829beb029e60a46935f4198e5
Átila Goes <atilagoes.tech@gmail.com>
2025-06-16 19:50:12 -0300

    Adiciona gráfico de linha ao Dashboard de Planos de Teste e ajusta a consulta para exibir a evolução mensal dos planos de teste.

--- template/app/control/SGPT/Graficos/LineChartView.php
+++ template/app/control/SGPT/Graficos/LineChartView.php
@@ -6,24 +6,48 @@ class LineChartView extends TPage
     {
         parent::__construct();
 
-        $html = new THtmlRenderer('app/resources/google_chart_base.html');
+        $html = new THtmlRenderer('app/resources/google_line_chart.html');
 
         $data = [];
-        $data[] = ['Day', 'Value 1', 'Value 2', 'Value 3'];
-        $data[] = ['Day 1', 120, 140, 160];
-        $data[] = ['Day 2', 100, 120, 140];
-        $data[] = ['Day 3', 140, 160, 180];
+        // Cabeçalho do gráfico
+        $data[] = ['Mês', 'Planos de Teste'];
+
+        try {
+            TTransaction::open('SGPT_DB');
+
+            $conn = TTransaction::get();
+
+            // Consulta que traz quantidade de planos de teste agrupados por mês
+            $result = $conn->query("
+                SELECT 
+                    TO_CHAR(pt.dt_criacao, 'YYYY-MM') AS mes,
+                    COUNT(pt.id_plano_teste) AS total_planos
+                FROM plano_teste pt
+                GROUP BY mes
+                ORDER BY mes
+            ");
+
+            foreach ($result as $row) {
+                $data[] = [
+                    $row['mes'],
+                    (int) $row['total_planos']
+                ];
+            }
+
+            TTransaction::close();
+        }
+        catch (Exception $e) {
+            new TMessage('error', 'Erro ao carregar dados: ' . $e->getMessage());
+        }
 
         $html->enableSection('main', [
-            'data'       => json_encode($data),
-            'width'      => '100%',
-            'height'     => '300px',
-            'title'      => 'Accesses by day',
-            'ytitle'     => 'Accesses',
-            'xtitle'     => 'Day',
-            'uniqid'     => uniqid(),
-            'chart_type' => 'LineChart',
-            'options'    => "" // se quiser, opções específicas do line chart
+            'data'   => json_encode($data),
+            'width'  => '100%',
+            'height' => '400px',
+            'title'  => 'Evolução dos Planos de Teste',
+            'ytitle' => 'Quantidade',
+            'xtitle' => 'Mês',
+            'uniqid' => uniqid()
         ]);
 
         $container = new TVBox;
