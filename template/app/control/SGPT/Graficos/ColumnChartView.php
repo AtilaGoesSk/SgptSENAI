29e0a3c8129adb83987e40e9074f3ac27ea1d46f
Átila Goes <atilagoes.tech@gmail.com>
2025-06-16 19:44:24 -0300

    Atualiza a classe ColumnChartView para exibir planos de teste por projeto e ajusta o DashboardPlanoTeste para incluir o gráfico de colunas.

--- template/app/control/SGPT/Graficos/ColumnChartView.php
+++ template/app/control/SGPT/Graficos/ColumnChartView.php
@@ -6,24 +6,49 @@ class ColumnChartView extends TPage
     {
         parent::__construct();
 
-        $html = new THtmlRenderer('app/resources/google_chart_base.html');
+        $html = new THtmlRenderer('app/resources/google_column_chart.html');
 
         $data = [];
-        $data[] = ['Day', 'Value 1', 'Value 2', 'Value 3'];
-        $data[] = ['Day 1', 100, 120, 140];
-        $data[] = ['Day 2', 120, 140, 160];
-        $data[] = ['Day 3', 140, 160, 180];
+        // Cabeçalho do gráfico
+        $data[] = ['Projeto', 'Planos de Teste'];
+
+        try {
+            TTransaction::open('SGPT_DB');
+
+            $conn = TTransaction::get();
+
+            // Consulta que traz a quantidade de planos de teste por projeto
+            $result = $conn->query("
+                SELECT 
+                    p.nm_projeto AS projeto,
+                    COUNT(pt.id_plano_teste) AS total_planos
+                FROM projeto_teste p
+                LEFT JOIN plano_teste pt ON pt.id_projeto = p.id_projeto
+                GROUP BY p.nm_projeto
+                ORDER BY total_planos DESC
+            ");
+
+            foreach ($result as $row) {
+                $data[] = [
+                    $row['projeto'],
+                    (int) $row['total_planos']
+                ];
+            }
+
+            TTransaction::close();
+        }
+        catch (Exception $e) {
+            new TMessage('error', 'Erro ao carregar dados: ' . $e->getMessage());
+        }
 
         $html->enableSection('main', [
-            'data'       => json_encode($data),
-            'width'      => '100%',
-            'height'     => '300px',
-            'title'      => 'Accesses by day',
-            'ytitle'     => 'Accesses',
-            'xtitle'     => 'Day',
-            'uniqid'     => uniqid(),
-            'chart_type' => 'ColumnChart', // tipo de gráfico do Google Charts
-            'options'    => "seriesType: 'bars'" // opções extras do gráfico
+            'data'   => json_encode($data),
+            'width'  => '100%',
+            'height' => '400px',
+            'title'  => 'Planos de Teste por Projeto',
+            'ytitle' => 'Quantidade',
+            'xtitle' => 'Projeto',
+            'uniqid' => uniqid()
         ]);
 
         $container = new TVBox;
